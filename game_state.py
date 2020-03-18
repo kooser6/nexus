2e7474dbba37fc9972248f3ef5d698938368aa2d
kooser6 <kooser6@users.noreply.github.com>
2020-03-18 07:26:54 -0400

    Add more logic.

--- game_state.py
+++ game_state.py
@@ -3,12 +3,26 @@ import state
 import threat
 
 class GameState:
-    def __init__(self, color, board, half_moves, castle_rights, enpassants):
+    def __init__(self, color, board, half_moves, castle_rights, enpassants, history):
+        """ Construct A New Game State """
 		self.board = board
 		self.color = color
         self.half_moves = half_moves
         self.castle_rights = castle_rights
         self.enpassants = enpassants
+        self.history = history
 
     def get_actions(self):
-		return move.get_moves(self.color, self.board, self.castle_rights, self.enpassants)
+        """ Get The Avaliable Actions """
+		return move.get_moves(self.board, self.color, self.castle_rights, self.enpassants)
+
+    def play_action(self, action):
+        """ Play The Action """
+        """                       """
+        """ Preform New Move Here """
+        """                       """
+		newState = GameState(newColor, newBoard, newHalfMoves, newCastleRights, newEnpassants)
+
+    def gameover(self):
+        """ Check To See If This Is A Final Position """
+        return state.is_gameover(self.color, self.board, self.half_moves, self.castle_rights, self.enpassants)
