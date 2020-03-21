44ed35114e6cca0184bab7ba453c537b4fa01902
kooser6 <kooser6@users.noreply.github.com>
2020-03-21 03:47:58 -0400

    Add FEN parser.

--- api.py
+++ api.py
@@ -2,3 +2,6 @@
 # and is released under the "GNU General Public License v3.0". Please see the LICENSE
 # file that should have been included as part of this package.
 #------------------------------------------------------------------------------------
+
+beta = "https://4player-beta.chess.com"
+main = "https://4player.chess.com"
