428bcabeb8eb734fd7b4848afb226012e845f1ea
kooser6 <48822466+kooser6@users.noreply.github.com>
2020-03-21 04:08:05 -0400

    Add engine wrapper.

---
+++ engine_wrapper.py
@@ -0,0 +1,39 @@
+# This file is part of Fisheater, A strong 4 player chess engine,
+# and is released under the "GNU General Public License v3.0". Please see the LICENSE
+# file that should have been included as part of this package.
+#------------------------------------------------------------------------------------
+
+class EngineWrapper:
+    def __init__(self, board, commands, options = None, silence_stderr = False):
+        pass
+
+    def set_time_control(self, game):
+        pass
+
+    def first_search(self, board, movetime):
+        pass
+
+    def search(self, board, wtime, btime, winc, binc):
+        pass
+
+    def print_stats(self):
+        pass
+
+    def name(self):
+        return self.engine.name
+
+    def quit(self):
+        self.engine.quit()
+
+    def print_handler_stats(self, info, stats):
+        for stat in stats:
+            if stat in info:
+                print("    {}: {}".format(stat, info[stat]))
+
+    def get_handler_stats(self, info, stats):
+        stats_str = []
+        for stat in stats:
+            if stat in info:
+                stats_str.append("{}: {}".format(stat, info[stat]))
+
+        return stats_str
