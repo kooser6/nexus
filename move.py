f7ce77ecfae893d0bf3c7ea5c59ddb870f081ad4
kooser6 <48822466+kooser6@users.noreply.github.com>
2020-03-21 01:49:14 -0400

    Add license.

--- move.py
+++ move.py
@@ -1,3 +1,8 @@
+  # This file is part of Fisheater, A strong 4 player chess engine,
+# and is released under the "GNU General Public License v3.0". Please see the LICENSE
+# file that should have been included as part of this package.
+#------------------------------------------------------------------------------------
+
 import state
 import threat
 
